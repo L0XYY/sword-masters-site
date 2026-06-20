e07c0efedb4aa6694c8b8f14be78f7345c6934b3
Loxy <77296384+L0XYY@users.noreply.github.com>
2026-06-20 07:51:55 +0100

    Delete api-config.js

--- api-config.js
+++
@@ -1,3 +0,0 @@
-// Paste your deployed API URL here after you deploy the Cloudflare Worker.
-// Example: https://swordmasters-api.yourname.workers.dev
-export const API_URL = 'PASTE_YOUR_API_URL_HERE';
